341900cf2c8586b1434cd122cf98dddc67cef9a8
copypasteonly <copypasteonly@pm.me>
2023-04-20 16:37:13 +0800

    Changed home to login page, and added another route to admin page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { AdminPage } from "./pages/AdminPage";
 import { ProductPage } from "./pages/ProductPage";
+import { LoginPage } from "./pages/LoginPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<AdminPage />}></Route>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/Admin" element={<AdminPage />}></Route>
         <Route path="/products/:productId" element={<ProductPage />}></Route>
       </Routes>
     </>
