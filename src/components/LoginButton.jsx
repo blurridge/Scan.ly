a59b8cddd7f12a85426c70bc6fcda9168a747442
Zach <zachriane01@gmail.com>
2023-04-23 23:21:56 +0800

    [FEAT][Zach] Implemented user checking in login

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleButton from "react-google-button";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { db } from "../firebase/config";
+import { collection, query, onSnapshot } from "firebase/firestore";
 
 export const LoginButton = () => {
-  const { googleLogin, user } = UserAuth();
+  const { googleLogin, user, logOut } = UserAuth();
+  const [adminList, setAdminList] = useState([]);
+  const [validUser, setValidUser] = useState(true);
   const navigate = useNavigate();
 
   const handleGoogleLogin = async () => {
@@ -16,20 +20,58 @@ export const LoginButton = () => {
   };
 
   useEffect(() => {
-    if(user !== null) {
-      navigate('/admin');
-    }
-  }, [user])
+    const q = query(collection(db, "users"));
+    const unsubscribe = onSnapshot(q, (snap) => {
+      const data = snap.docs.map((doc) => ({ ...doc.data() }));
+      const emails = data.map((data) => data.email);
+      setAdminList(emails);
+    });
+    return () => unsubscribe();
+  }, []);
 
-  return (
-    <>
-      <GoogleButton
-        data-te-ripple-init
-        type="light"
-        onClick={() => {
-          handleGoogleLogin();
-        }}
-      />
-    </>
-  );
+  useEffect(() => {
+    const checkIfUserExists = async () => {
+      if (
+        user !== null &&
+        adminList.length !== 0 &&
+        adminList.includes(user.email)
+      ) {
+        setValidUser(true);
+        navigate("/admin");
+      } else if (user !== null && user.hasOwnProperty("email")) {
+        setValidUser(false);
+        await logOut();
+      }
+    };
+    checkIfUserExists();
+  }, [user]);
+
+  if (validUser) {
+    return (
+      <>
+        <GoogleButton
+          data-te-ripple-init
+          type="light"
+          onClick={() => {
+            handleGoogleLogin();
+          }}
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <GoogleButton
+          data-te-ripple-init
+          type="light"
+          onClick={() => {
+            handleGoogleLogin();
+          }}
+        />
+        <span class="font-title text-red-600">
+          Invalid user. Please try again.
+        </span>
+      </>
+    );
+  }
 };
