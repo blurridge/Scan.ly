3f6bc5e92aab7fb0803bd40a8a5466f279131055
Zach <zachriane01@gmail.com>
2023-04-24 11:51:58 +0800

    [CHORE][Zach] Added link from Vercel for QR

--- src/utils/generateProductTag.js
+++ src/utils/generateProductTag.js
@@ -1,14 +1,12 @@
 import { jsPDF } from "jspdf";
 
 export const generateProductTag = (product) => {
-    console.log(product);
   const doc = new jsPDF({
     orientation: "vertical",
     unit: "in",
     format: [2, 3],
   });
   doc.setFontSize(12);
-  console.log(product.qrData)
   const splitTitle = doc.splitTextToSize(product.name, 1);
   doc.addImage(product.qrData, "JPEG", 0.5, 0.3, 1, 1);
   doc.text(0.5, 1.7, splitTitle);
--- src/utils/generateQrCode.js
+++ src/utils/generateQrCode.js
@@ -2,7 +2,7 @@ import { ref, uploadString } from "firebase/storage";
 import { storage } from "../firebase/config";
 import QRCode from "qrcode";
 
-const STARTING_URL = "http://localhost:3000/products/";
+const STARTING_URL = "https://scanly-app.vercel.app/products/";
 export const generateQrCode = async (id) => {
   const url = STARTING_URL + id;
   const storageRef = ref(storage, `${id}-qr.jpg`);
