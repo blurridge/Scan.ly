c5c4e2a71cff5dc67c099386a315e1285a3a89eb
Zach <zachriane01@gmail.com>
2023-05-04 15:14:10 +0800

    [STYLE][Zach] Added responsiveness for User Management

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -7,19 +7,15 @@ import { DeleteUserDialog } from "../modals/DeleteUserDialog";
 export const UserRow = ({ user, users }) => {
   return (
     <>
-      <div class="rounded-3xl bg-gray-200 px-10 py-5 flex items-center justify-between mb-5">
+      <div class="rounded-3xl bg-gray-200 px-5 lg:px-10 py-5 flex items-center justify-between mb-5">
         <div class="flex gap-10 items-center">
-          <span class="font-title text-lg xl:text-2xl font-bold mr-5 w-48 xl:w-72">
+          <span class="break-words font-title text-sm lg:text-lg xl:text-2xl font-bold w-24 md:w-60 lg:w-96">
             {user.email}
           </span>
         </div>
         <div class="flex ml-10 gap-2 xl:gap-5 items-center">
           <EditButton id={"e" + user.id} />
-          <EditUserDialog
-            user={user}
-            id={"e" + user.id}
-            users={users}
-          />
+          <EditUserDialog user={user} id={"e" + user.id} users={users} />
           <DeleteButton id={"d" + user.id} />
           <DeleteUserDialog user={user} id={"d" + user.id} />
         </div>
--- src/components/UserStats.jsx
+++ src/components/UserStats.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const UserStats = ({ users }) => {
   return (
     <>
-      <div class="py-3 xl:py-7 px-14 xl:px-20 bg-gray-200 rounded-3xl flex gap-20 items-center justify-center">
+      <div class="py-3 xl:py-7 px-10 md:px-14 xl:px-20 bg-gray-200 rounded-3xl flex gap-20 items-center justify-center">
         <div class="flex flex-col gap-2 text-center">
           <span class="font-title font-bold text-lg lg:text-3xl xl:text-4xl">
             Total Users: {users.length}
